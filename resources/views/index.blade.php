2899a0287a99e4b6b50a79c448433e93f018245e
Warex <warex@junix.de>
2022-02-10 19:38:11 +0100

    jup

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,7 +8,7 @@
         <div class="bg-white shadow rounded">
             <img src="https://picsum.photos/800/350?random={{rand(0,100)}}" alt="Random image" class="rounded-t">
             <div class="p-5">
-                <div class="font-bold text-xs text-blue-700">{{ $post->created_at }}</div>
+                <div class="font-bold text-xs text-blue-700">qqq{{ $post->created_at }}</div>
                 <div class="text-3xl text-blue-900">{{ $post->title }}</div>
                 <div class="text-lg text-gray-700">{{ $post->body }}
                 </div>
